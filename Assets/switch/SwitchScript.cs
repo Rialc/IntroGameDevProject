45a611204d98c22404bab4da2ffb15bb200c6d83
Rialc <clair.m.hayden@gmail.com>
2017-04-04 02:10:09 -0400

    ...All the files this time Final
    Won't be making that mistake again

---
+++ Assets/switch/SwitchScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour {
+    public GameObject LinkedDoor;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+
+            LinkedDoor.GetComponent<firewheelscript>().rotation = new Vector3(0,0,-1);
+            GetComponent<SpriteRenderer>().color = Color.grey;
+
+
+        }
+
+    }
+}
